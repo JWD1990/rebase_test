2c6b76b12dfdec8116bd36e5c3fec23c456086e1
Alexandr <san4azms@gmail.com>
2023-02-08 20:32:37 +0600

    Change dev 2

--- main.py
+++ main.py
@@ -7,9 +7,12 @@
 def print_hi(name):
     pass
 
+def print_hi222(name):
+    pass
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print_hi()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
